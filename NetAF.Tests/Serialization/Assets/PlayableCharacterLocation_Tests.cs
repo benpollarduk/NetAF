8059d124059386d587e670a8d655043f9374aeae
ben_singer <ben@singerinstruments.com>
2024-11-02 23:08:08 +0000

    Added missing test

---
+++ NetAF.Tests/Serialization/Assets/PlayableCharacterLocation_Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetAF.Logic;
+using NetAF.Serialization.Assets;
+
+namespace NetAF.Tests.Serialization.Assets
+{
+    [TestClass]
+    public class PlayableCharacterLocation_Tests
+    {
+        [TestMethod]
+        public void GivenSerialization_WhenFromSerialization_ThenRestoredCorrectly()
+        {
+            PlayableCharacterLocation location = new("a", "b", "c");
+            PlayableCharacterLocationSerialization serialization = new(location);
+
+            var result = PlayableCharacterLocation.FromSerialization(serialization);
+
+            Assert.AreEqual("a", result.PlayerIdentifier);
+            Assert.AreEqual("b", result.RegionIdentifier);
+            Assert.AreEqual("c", result.RoomIdentifier);
+        }
+    }
+}
