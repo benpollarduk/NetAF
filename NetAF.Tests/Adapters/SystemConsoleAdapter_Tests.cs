d10ae0bda5f490f6e84e27edc05b28e8768c6b55
ben_singer <ben@singerinstruments.com>
2024-11-14 15:35:50 +0000

    Added additional tests

---
+++ NetAF.Tests/Adapters/SystemConsoleAdapter_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using NetAF.Assets.Characters;
+using NetAF.Assets.Locations;
+using NetAF.Logic;
+using NetAF.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetAF.Adapters;
+using NetAF.Rendering.FrameBuilders.Console;
+
+namespace NetAF.Tests.Logic
+{
+    [TestClass]
+    public class SystemConsoleAdapter_Tests
+    {
+        [TestMethod]
+        public void GivenNoConsoleAccess_WhenRenderFrame_ThenIOExceptionThrown()
+        {
+            Assert.ThrowsException<IOException>(() =>
+            {
+                RegionMaker regionMaker = new(string.Empty, string.Empty);
+                Room room = new("Room", string.Empty);
+                regionMaker[0, 0, 0] = room;
+                OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
+                var game = Game.Create(new(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, ConsoleGameConfiguration.Default);
+                var aboutFrame = new ConsoleAboutFrameBuilder(new NetAF.Rendering.FrameBuilders.GridStringBuilder()).Build("abc", game.Invoke(), 50, 50);
+                var adapter = new SystemConsoleAdapter();
+
+                adapter.RenderFrame(aboutFrame);
+            });
+        }
+    }
+}
--- NetAF.Tests/Logic/Modes/ReactionMode_Tests.cs
+++ NetAF.Tests/Logic/Modes/ReactionMode_Tests.cs
@@ -4,6 +4,8 @@ using NetAF.Assets.Characters;
 using NetAF.Assets.Locations;
 using NetAF.Utilities;
 using NetAF.Logic.Modes;
+using NetAF.Commands.Scene;
+using NetAF.Commands.Global;
 
 namespace NetAF.Tests.Logic.Modes
 {
@@ -11,7 +13,7 @@ namespace NetAF.Tests.Logic.Modes
     public class ReactionMode_Tests
     {
         [TestMethod]
-        public void GivenNew_WhenRender_ThenReturnAborted()
+        public void GivenNew_WhenRenderWithEmptyString_ThenReturnAborted()
         {
             RegionMaker regionMaker = new(string.Empty, string.Empty);
             Room room = new(string.Empty, string.Empty);
@@ -19,6 +21,36 @@ namespace NetAF.Tests.Logic.Modes
             OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
             var game = Game.Create(new(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
             var mode = new ReactionMode(string.Empty, string.Empty);
+
+            var result = mode.Render(game);
+
+            Assert.AreEqual(RenderState.Aborted, result);
+        }
+
+        [TestMethod]
+        public void GivenNew_WhenRenderWithSuccessfulMove_ThenReturnAborted()
+        {
+            RegionMaker regionMaker = new(string.Empty, string.Empty);
+            Room room = new(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(new(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            var mode = new ReactionMode(string.Empty, Move.SuccessfulMove);
+
+            var result = mode.Render(game);
+
+            Assert.AreEqual(RenderState.Aborted, result);
+        }
+
+        [TestMethod]
+        public void GivenNew_WhenRenderWithSuccessfulEnd_ThenReturnAborted()
+        {
+            RegionMaker regionMaker = new(string.Empty, string.Empty);
+            Room room = new(string.Empty, string.Empty);
+            regionMaker[0, 0, 0] = room;
+            OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(new(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            var mode = new ReactionMode(string.Empty, End.SuccessfulEnd);
 
             var result = mode.Render(game);
 
