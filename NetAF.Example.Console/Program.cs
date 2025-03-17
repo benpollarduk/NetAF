ccd08e7ff7f3cb44b6210131196c52d75b3af988
ben_pollard <ben@singerinstruments.com>
2025-03-17 15:23:11 +0000

    Simplified console

--- NetAF.Example.Console/Program.cs
+++ NetAF.Example.Console/Program.cs
@@ -3,13 +3,5 @@ using NetAF.Logic;
 using NetAF.Rendering.FrameBuilders;
 using NetAF.Targets.Console;
 
-try
-{
-    var creator = ExampleGame.Create(new GameConfiguration(new ConsoleAdapter(), FrameBuilderCollections.Console, new(80, 50)));
-    GameExecutor.Execute(creator, new ConsoleExecutionController());
-}
-catch (Exception e)
-{
-    Console.WriteLine($"Exception caught running demo: {e.Message}");
-    Console.ReadKey();
-}
+var creator = ExampleGame.Create(new GameConfiguration(new ConsoleAdapter(), FrameBuilderCollections.Console, new(80, 50)));
+GameExecutor.Execute(creator, new ConsoleExecutionController());
