441390b7d4dea34dd8d9b4d256bd5bed693ffdf6
ben_pollard <ben@singerinstruments.com>
2025-03-28 16:56:56 +0000

    Added text adapter

---
+++ NetAF.Tests/Targets/Text/TextAdapter_Tests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetAF.Assets.Characters;
+using NetAF.Assets.Locations;
+using NetAF.Assets;
+using NetAF.Logic;
+using NetAF.Targets.Html;
+using NetAF.Utilities;
+using NetAF.Targets.Html.Rendering.FrameBuilders;
+using NetAF.Targets.Html.Rendering;
+using NetAF.Targets.Console.Rendering.FrameBuilders;
+using NetAF.Targets.Console.Rendering;
+
+namespace NetAF.Tests.Targets.Text
+{
+    [TestClass]
+    public class TextAdapter_Tests
+    {
+        [TestMethod]
+        public void GivenFrame_WhenRender_ThenRendered()
+        {
+            RegionMaker regionMaker = new(string.Empty, string.Empty);
+            Item item = new(string.Empty, string.Empty) { IsPlayerVisible = false };
+            Room room = new(string.Empty, string.Empty, null, [item]);
+            regionMaker[0, 0, 0] = room;
+            OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            HtmlBuilder htmlBuilder = new();
+            var frame = new HtmlReactionFrameBuilder(htmlBuilder).Build("A", "B", false, new(80, 50));
+            TestPresenter presenter = new();
+            HtmlAdapter adapter = new(presenter);
+            adapter.Setup(game);
+
+            adapter.RenderFrame(frame);
+
+            Assert.IsTrue(!string.IsNullOrEmpty(presenter.ToString()));
+        }
+
+        [TestMethod]
+        public void GivenIConsoleFrame_WhenRender_ThenRendered()
+        {
+            RegionMaker regionMaker = new(string.Empty, string.Empty);
+            Item item = new(string.Empty, string.Empty) { IsPlayerVisible = false };
+            Room room = new(string.Empty, string.Empty, null, [item]);
+            regionMaker[0, 0, 0] = room;
+            OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            GridStringBuilder gridStringBuilder = new();
+            var frame = new ConsoleReactionFrameBuilder(gridStringBuilder).Build("A", "B", false, new(80, 50));
+            TestPresenter presenter = new();
+            HtmlAdapter adapter = new(presenter);
+            adapter.Setup(game);
+
+            adapter.RenderFrame(frame);
+
+            Assert.IsTrue(!string.IsNullOrEmpty(presenter.ToString()));
+        }
+    }
+}
---
+++ NetAF/Targets/Text/TextAdapter.cs
@@ -0,0 +1,80 @@
+﻿using NetAF.Assets;
+using NetAF.Logic;
+using NetAF.Rendering;
+using NetAF.Targets.Console.Rendering;
+using System.Text;
+
+namespace NetAF.Targets.Text
+{
+    /// <summary>
+    /// Provides an adapter for text.
+    /// </summary>
+    /// <param name="presenter">The presenter to use for presenting frames.</param>
+    public sealed class TextAdapter(IFramePresenter presenter) : IIOAdapter
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get the game.
+        /// </summary>
+        public Game Game { get; private set; }
+
+        #endregion
+
+        #region StaticMethods
+
+        /// <summary>
+        /// Convert an instance of IConsoleFrame to a string.
+        /// </summary>
+        /// <param name="frame">The frame to convert.</param>
+        /// <param name="size">The size of the frame.</param>
+        /// <returns>The converted string.</returns>
+        internal static string Convert(IConsoleFrame frame, Size size)
+        {
+            StringBuilder stringBuilder = new();
+
+            for (var row = 0; row < size.Height; row++)
+            {
+                for (var column = 0; column < size.Width; column++)
+                {
+                    var cell = frame.GetCell(column, row);
+                    var character = cell.Character == 0 ? ' ' : cell.Character;
+                    stringBuilder.Append(character);
+                }
+
+                if (row < size.Height - 1)
+                    stringBuilder.AppendLine();
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        #endregion
+
+        #region Implementation of IIOAdapter
+
+        /// <summary>
+        /// Setup for a game.
+        /// </summary>
+        /// <param name="game">The game to set up for.</param>
+        public void Setup(Game game)
+        {
+            Game = game;
+        }
+
+        /// <summary>
+        /// Render a frame.
+        /// </summary>
+        /// <param name="frame">The frame to render.</param>
+        public void RenderFrame(IFrame frame)
+        {
+            // convert the console frame to text frame if possible
+            if (frame is IConsoleFrame ansiFrame)
+                presenter.Present(Convert(ansiFrame, Game.Configuration.DisplaySize));
+            else
+                frame.Render(presenter);
+        }
+
+        #endregion
+    }
+}
