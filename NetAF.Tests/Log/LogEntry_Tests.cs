f26ca6d21e3ed4e07c4145e386b86e8f6030f781
ben_pollard <ben@singerinstruments.com>
2025-04-08 15:01:07 +0100

    Added previously ignored files

---
+++ NetAF.Tests/Log/LogEntry_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetAF.Log;
+using NetAF.Serialization.Assets;
+
+namespace NetAF.Tests.Log
+{
+    [TestClass]
+    public class LogEntry_Tests
+    {
+        [TestMethod]
+        public void GivenSerialization_WhenFromSerialization_ThenRestoredCorrectly()
+        {
+            LogEntry entry = new("a", "b");
+            entry.Expire();
+            LogEntrySerialization serialization = LogEntrySerialization.FromLogEntry(entry);
+
+            var result = LogEntry.FromSerialization(serialization);
+
+            Assert.AreEqual("a", result.Name);
+            Assert.AreEqual("b", result.Content);
+            Assert.IsTrue(result.HasExpired);
+        }
+    }
+}
---
+++ NetAF/Log/LogEntry.cs
@@ -0,0 +1,75 @@
+﻿using NetAF.Serialization;
+using NetAF.Serialization.Assets;
+
+namespace NetAF.Log
+{
+    /// <summary>
+    /// Provides a log entry.
+    /// </summary>
+    /// <param name="name">The name of the entry.</param>
+    /// <param name="content">The content of the entry.</param>
+    public class LogEntry(string name, string content) : IRestoreFromObjectSerialization<LogEntrySerialization>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get the name of this entry.
+        /// </summary>
+        public string Name { get; private set; } = name;
+
+        /// <summary>
+        /// Get the content of this entry.
+        /// </summary>
+        public string Content { get; private set; } = content;
+
+        /// <summary>
+        /// Get if this entry has expired.
+        /// </summary>
+        public bool HasExpired { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Mark this entry as expired.
+        /// </summary>
+        public void Expire()
+        {
+            HasExpired = true;
+        }
+
+        #endregion
+
+        #region StaticMethods
+
+        /// <summary>
+        /// Create a new instance of LogEntry from a serialization.
+        /// </summary>
+        /// <param name="serialization">The serialization.</param>
+        /// <returns>The log entry.</returns>
+        public static LogEntry FromSerialization(LogEntrySerialization serialization)
+        {
+            LogEntry entry = new(string.Empty, string.Empty);
+            ((IRestoreFromObjectSerialization<LogEntrySerialization>)entry).RestoreFrom(serialization);
+            return entry;
+        }
+
+        #endregion
+
+        #region Implementation of IRestoreFromObjectSerialization<LogEntrySerialization>
+
+        /// <summary>
+        /// Restore this object from a serialization.
+        /// </summary>
+        /// <param name="serialization">The serialization to restore from.</param>
+        void IRestoreFromObjectSerialization<LogEntrySerialization>.RestoreFrom(LogEntrySerialization serialization)
+        {
+            Name = serialization.Name;
+            Content = serialization.Content;
+            HasExpired = serialization.HasExpired;
+        }
+
+        #endregion
+    }
+}
