23b8238649cdb711bc8bb17fb9d4224435c85d43
ben_pollard <ben@singerinstruments.com>
2025-05-07 15:35:26 +0100

    Added class, docs and tests

---
+++ NetAF.Tests/Assets/MultiConditionalDescription_Tests.cs
@@ -0,0 +1,39 @@
+﻿using NetAF.Assets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetAF.Tests.Assets
+{
+    [TestClass]
+    public class MultiConditionalDescription_Tests
+    {
+        [TestMethod]
+        public void GivenGetDescription_WhenNull_ThenReturnFallbackDescription()
+        {
+            var conditional = new MultiConditionalDescription("A", null);
+
+            var result = conditional.GetDescription();
+
+            Assert.AreEqual("A", result);
+        }
+
+        [TestMethod]
+        public void GivenGetDescription_WhenCondition1IsTrue_ThenReturnCondition1Description()
+        {
+            var conditional = new MultiConditionalDescription("A", new DescribedCondition(() => true, "B"));
+
+            var result = conditional.GetDescription();
+
+            Assert.AreEqual("B", result);
+        }
+
+        [TestMethod]
+        public void GivenGetDescription_WhenAllConditionsAreFalse_ThenReturnFallbackDescription()
+        {
+            var conditional = new MultiConditionalDescription("A", new DescribedCondition(() => false, "B"));
+
+            var result = conditional.GetDescription();
+
+            Assert.AreEqual("A", result);
+        }
+    }
+}
