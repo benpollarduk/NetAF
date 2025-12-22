c1cacb5a560cfbed339d20be9f9b91387bfcc3ae
ben_pollard <ben@singerinstruments.com>
2025-12-22 10:08:56 +0000

    Tidy

--- NetAF.Example.Console/Program.cs
+++ NetAF.Example.Console/Program.cs
@@ -3,5 +3,5 @@ using NetAF.Logic;
 using NetAF.Rendering.FrameBuilders;
 using NetAF.Targets.Console;
 
-var creator = ExampleGame.Create(new GameConfiguration(new ConsoleAdapter(), FrameBuilderCollections.Console, new(80, 50), StartModes.TitleScreen));
+var creator = ExampleGame.Create(new GameConfiguration(new ConsoleAdapter(), FrameBuilderCollections.Console, new(80, 50)));
 GameExecutor.Execute(creator, new ConsoleExecutionController());
