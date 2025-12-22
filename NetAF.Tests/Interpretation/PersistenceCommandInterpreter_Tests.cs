75529364650cf05de2991a090fc1ba9319112513
ben_pollard <ben@singerinstruments.com>
2025-12-22 14:07:34 +0000

    Added persistence interpreter

---
+++ NetAF.Tests/Interpretation/PersistenceCommandInterpreter_Tests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetAF.Assets;
+using NetAF.Assets.Characters;
+using NetAF.Assets.Locations;
+using NetAF.Interpretation;
+using NetAF.Logic;
+using NetAF.Logic.Modes;
+
+namespace NetAF.Tests.Interpretation
+{
+    [TestClass]
+    public class PersistenceCommandInterpreter_Tests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            overworld = new Overworld(Identifier.Empty, Description.Empty);
+            var region = new Region(Identifier.Empty, Description.Empty);
+            region.AddRoom(new(Identifier.Empty, Description.Empty, [new Exit(Direction.North)]), 0, 0, 0);
+            region.AddRoom(new(Identifier.Empty, Description.Empty, [new Exit(Direction.South)]), 0, 1, 0);
+            overworld.AddRegion(region);
+            region.Enter();
+        }
+
+        private Overworld overworld;
+
+        [TestMethod]
+        public void GivenEmptyString_WhenInterpret_ThenReturnFalse()
+        {
+            var interpreter = new PersistenceCommandInterpreter();
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworld, new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+
+            var result = interpreter.Interpret(string.Empty, game);
+
+            Assert.IsFalse(result.WasInterpretedSuccessfully);
+        }
+
+        [TestMethod]
+        public void GivenLoad_WhenInterpret_ThenReturnTrue()
+        {
+            var interpreter = new PersistenceCommandInterpreter();
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworld, new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+
+            var result = interpreter.Interpret(NetAF.Commands.Persistence.Load.CommandHelp.Command, game);
+
+            Assert.IsTrue(result.WasInterpretedSuccessfully);
+        }
+
+        [TestMethod]
+        public void GivenSave_WhenInterpret_ThenReturnTrue()
+        {
+            var interpreter = new PersistenceCommandInterpreter();
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworld, new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+
+            var result = interpreter.Interpret(NetAF.Commands.Persistence.Save.CommandHelp.Command, game);
+
+            Assert.IsTrue(result.WasInterpretedSuccessfully);
+        }
+
+        [TestMethod]
+        public void GivenInterpreter_WhenGetSupportedCommands_ThenReturnArrayWithSomeItems()
+        {
+            var interpreter = new PersistenceCommandInterpreter();
+
+            var result = interpreter.SupportedCommands;
+
+            Assert.IsTrue(result.Length > 0);
+        }
+
+        [TestMethod]
+        public void GivenInterpreter_WhenGetContextualCommandHelp_ThenReturnArrayWithSomeItems()
+        {
+            var interpreter = new PersistenceCommandInterpreter();
+            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworld, new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            game.ChangeMode(new SceneMode());
+
+            var result = interpreter.GetContextualCommandHelp(game);
+
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
