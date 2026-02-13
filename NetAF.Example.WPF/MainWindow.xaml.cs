ff1d31d9a6cd4a28c1cb641e4c66579365015885
ben_pollard <ben@singerinstruments.com>
2026-02-13 11:56:03 +0000

    Included WPF example

---
+++ NetAF.Example.WPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using NetAF.Events;
+using NetAF.Logic;
+using NetAF.Rendering.FrameBuilders;
+using NetAF.Targets.Markup;
+using System.Windows;
+
+namespace NetAF.Example.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EventBus.Subscribe<GameStarted>(x => Title = x.Game.Info.Name);
+
+            var configuration = new GameConfiguration(new MarkupAdapter(Terminal), FrameBuilderCollections.Markup, NetAF.Assets.Size.Dynamic);
+            GameExecutor.Execute(ExampleGame.Create(configuration));
+        }
+
+        #endregion
+    }
+}
