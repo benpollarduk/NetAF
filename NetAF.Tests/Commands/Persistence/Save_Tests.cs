095504107d238d0e565bac6cfbd4b271d995fe6b
benpollarduk <bajpollard@gmail.com>
2026-03-22 17:44:20 +0000

    Added resource manager, updated commands

--- NetAF.Tests/Commands/Persistence/Save_Tests.cs
+++ NetAF.Tests/Commands/Persistence/Save_Tests.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NetAF.Logic;
+using NetAF.Assets;
 using NetAF.Assets.Characters;
 using NetAF.Assets.Locations;
-using NetAF.Assets;
+using NetAF.Commands;
+using NetAF.Commands.Persistence;
+using NetAF.Logic;
+using NetAF.Persistence;
 using NetAF.Utilities;
 using System.IO;
-using NetAF.Commands.Persistence;
-using NetAF.Commands;
 
 namespace NetAF.Tests.Commands.Persistence
 {
@@ -14,19 +15,42 @@ namespace NetAF.Tests.Commands.Persistence
     public class Save_Tests
     {
         [TestMethod]
-        public void GivenValidPath_WhenInvoke_ThenInform()
+        public void GivenEmptyString_WhenInvoke_ThenError()
         {
             var regionMaker = new RegionMaker(string.Empty, string.Empty);
             var item = new Item(string.Empty, string.Empty) { IsPlayerVisible = false };
             var room = new Room(string.Empty, string.Empty, null, [item]);
             regionMaker[0, 0, 0] = room;
             var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
-            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
-            var path = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
-            var command = new Save(path);
+            var game = Game.Create(new GameInfo("Test", string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+
+            var command = new Save(string.Empty);
 
             var result = command.Invoke(game);
             
+            Assert.AreEqual(ReactionResult.Error, result.Result);
+        }
+
+        [TestMethod]
+        public void GivenName_WhenInvoke_ThenInform()
+        {
+            var regionMaker = new RegionMaker(string.Empty, string.Empty);
+            var item = new Item(string.Empty, string.Empty) { IsPlayerVisible = false };
+            var room = new Room(string.Empty, string.Empty, null, [item]);
+            regionMaker[0, 0, 0] = room;
+            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
+            var game = Game.Create(new GameInfo("Test", string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            var name = "Test";
+                
+            var tempDir = Directory.CreateTempSubdirectory();
+            RestorePointManager.RootDirectory = tempDir.FullName;
+
+            RestorePointManager.Save(game, name, out _);
+            var command = new Save(name);
+            var path = RestorePointManager.GetFilePath(game, name);
+
+            var result = command.Invoke(game);
+
             var fileExists = File.Exists(path);
 
             if (fileExists)
@@ -36,40 +60,18 @@ namespace NetAF.Tests.Commands.Persistence
         }
 
         [TestMethod]
-        public void GivenInvalidPath_WhenInvoke_ThenError()
-        {
-            var regionMaker = new RegionMaker(string.Empty, string.Empty);
-            var item = new Item(string.Empty, string.Empty) { IsPlayerVisible = false };
-            var room = new Room(string.Empty, string.Empty, null, [item]);
-            regionMaker[0, 0, 0] = room;
-            var overworldMaker = new OverworldMaker(string.Empty, string.Empty, regionMaker);
-            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
-            var path = Path.Combine("abc");
-            var command = new Save(path);
-
-            var result = command.Invoke(game);
-
-            var fileExists = File.Exists(path);
-
-            if (fileExists)
-                File.Delete(path);
-
-            Assert.AreEqual(ReactionResult.Error, result.Result);
-        }
-
-        [TestMethod]
-        public void GivenGame_WhenGetPrompts_ThenEmptyArray()
+        public void GivenGame_WhenGetPrompts_ThenArrayWithSingleEntry()
         {
             RegionMaker regionMaker = new(string.Empty, string.Empty);
             Room room = new(string.Empty, string.Empty);
             regionMaker[0, 0, 0] = room;
             OverworldMaker overworldMaker = new(string.Empty, string.Empty, regionMaker);
-            var game = Game.Create(new GameInfo(string.Empty, string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
+            var game = Game.Create(new GameInfo("Test", string.Empty, string.Empty), string.Empty, AssetGenerator.Retained(overworldMaker.Make(), new PlayableCharacter(string.Empty, string.Empty)), GameEndConditions.NoEnd, TestGameConfiguration.Default).Invoke();
             var command = new Save(string.Empty);
 
             var result = command.GetPrompts(game);
 
-            Assert.IsEmpty(result);
+            Assert.HasCount(1, result);
         }
     }
 }
