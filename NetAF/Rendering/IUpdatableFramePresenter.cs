cb90cca0905d61809a5edc78315495c7cde5585f
benpollarduk <bajpollard@gmail.com>
2026-03-27 19:44:18 +0000

    Added updateable frame presenter

---
+++ NetAF/Rendering/IUpdatableFramePresenter.cs
@@ -0,0 +1,14 @@
+﻿namespace NetAF.Rendering
+{
+    /// <summary>
+    /// Represents an object that can present a frame.
+    /// </summary>
+    public interface IUpdatableFramePresenter : IFramePresenter
+    {
+        /// <summary>
+        /// Present an updated frame.
+        /// </summary>
+        /// <param name="frame">The udated frame to write, as a string.</param>
+        void PresentUpdate(string frame);
+    }
+}
